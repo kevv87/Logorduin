c8488b61d9bb9e15049a73669cb0f9bb9dd16edc
Marlon Alejandro Vega <marlon.vega@xerasistemas.net>
2020-07-01 23:24:14 -0600

    Creada estructura básica de la aplicación

--- src/Gui/Main.java
+++ src/Gui/Main.java
@@ -1,4 +1,40 @@
 package Gui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Clase de interfaz principal.
+ */
+public class Main extends Application {
+
+    /**
+     * Método para iniciar la aplicación.
+     * @param args Argumentos del programa.
+     */
+    public static void main(String[] args) {
+        launch(Main.class);
+    }
+
+    /**
+     * Método para configurar la ventana principal de la aplicación.
+     * @param stage Instancia de la ventana principal.
+     */
+    @Override
+    public void start(Stage stage) {
+        /*
+        Agregar la siguiente línea en configuración de ejecución, en campo VM Options.
+        --module-path "Ruta a la carpeta lib de JavaFx" --add-modules javafx.controls,javafx.fxml
+         */
+
+
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        stage.setTitle("Logorduin");
+        stage.setMaximized(true);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
